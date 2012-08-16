63a516dbea9d021ac4d549a3accad44e524fb642
Timothy Andrew <mail@timothyandrew.net>
2012-08-16 11:41:37 +0530

    Survey model validates its fields.

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Survey do
+  it { should respond_to :name }
+  it { should respond_to :expiry_date }
+  it { should respond_to :description }
+
   context "when validating" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :expiry_date }
