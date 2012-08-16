de725f65b7e838990ca4405df7996cc136b5c179
Timothy Andrew <mail@timothyandrew.net>
2012-08-16 21:32:59 +0530

    Add comment with Railscasts link to link_to_add_question_fields.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
-  def link_to_add_question_fields(name, f)
+  def link_to_add_question_fields(name, f)  
+    # http://railscasts.com/episodes/196-nested-model-form-revised
     field = f.semantic_fields_for(:questions) do |builder|
       render('questions/new', :f => builder)
     end
