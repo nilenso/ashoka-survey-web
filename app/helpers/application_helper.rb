cdbe8e3c667e8b6069a48764277bf39630fc4622
Timothy Andrew <mail@timothyandrew.net>
2012-08-16 19:00:58 +0530

    Add a helper method to insert a link which allows the creation of multiple questions.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def link_to_add_question_fields(name, f)
+    field = f.semantic_fields_for(:questions) do |builder|
+      render('questions/new', :f => builder)
+    end
+    link_to(name, "#", :class => "add_question_field", :data => { :field => field.gsub("\n", '')} )
+  end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+end
