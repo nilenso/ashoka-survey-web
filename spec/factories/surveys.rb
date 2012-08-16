e23638dfd74e7e1874c3b897fd9ca81c52a6dd67
Timothy Andrew <mail@timothyandrew.net>
2012-08-16 11:35:07 +0530

    Setup factorygirl and Spork.

---
+++ spec/factories/surveys.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :survey do
+    sequence(:name) { |n| "name_#{n}" }
+    expiry_date "2012-08-16"
+    description "MyText"
+  end
+end
