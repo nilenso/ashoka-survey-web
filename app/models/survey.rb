f464de604ec09312edd43fbcf339717189a3a292
Timothy Andrew <mail@timothyandrew.net>
2012-08-16 11:41:15 +0530

    Survey validates presence of expiry date.

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,3 @@
 class Survey < ActiveRecord::Base
-  validates_presence_of :name
+  validates_presence_of :name, :expiry_date
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe Survey do
   context "when validating" do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :expiry_date }
   end
 end
