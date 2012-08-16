fe6509a67588ac3abe66a8171ea6dc66512f79ad
Timothy Andrew <mail@timothyandrew.net>
2012-08-16 18:37:13 +0530

    Make Survey model accept nested attributes for Questions.

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,7 +1,8 @@
 # Collection of questions
 
 class Survey < ActiveRecord::Base
-  attr_accessible :name, :expiry_date, :description
+  attr_accessible :name, :expiry_date, :description, :questions_attributes
   validates_presence_of :name, :expiry_date
   has_many :questions
+  accepts_nested_attributes_for :questions
 end
