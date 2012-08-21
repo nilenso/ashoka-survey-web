0850489e5723b13b5bd6ce72743bc3588c1768f9
Timothy Andrew <mail@timothyandrew.net>
2012-08-21 23:44:08 +0530

    Added dependent => :destroy for a Question's answers as well.
    Missed this in 52b8f02

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,5 +4,5 @@ class Question < ActiveRecord::Base
   belongs_to :survey
   attr_accessible :content
   validates_presence_of :content
-  has_many :answers
+  has_many :answers, :dependent => :destroy
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe Question do
   it { should respond_to :content }
   it { should belong_to :survey }
+  it { should have_many(:answers).dependent(:destroy) }
   it { should validate_presence_of :content }
 end
