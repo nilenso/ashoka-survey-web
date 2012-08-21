27c799aa1b9254dd7df377194d9d623532f807c4
srihari <srihari@srihari-Inspiron-N5010.(none)>
2012-08-21 11:54:43 +0530

    Surveys have many responses. Added number of responses to table in Surverys#index

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -4,5 +4,6 @@ class Survey < ActiveRecord::Base
   attr_accessible :name, :expiry_date, :description, :questions_attributes
   validates_presence_of :name, :expiry_date
   has_many :questions
+  has_many :responses
   accepts_nested_attributes_for :questions
 end
