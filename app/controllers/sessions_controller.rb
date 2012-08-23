84fc49231180a395789b531379fc082e9cab20ff
Smit Shah <who828@gmail.com>
2012-08-23 16:54:17 +0530

    Added create and destroy action to Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+
+  def create
+    auth = request.env["omniauth.auth"]
+    session[:user_id] = auth['uid']
+    session[:access_token] = auth['credentials']['token']
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    session[:access_token] = nil
+    redirect_to(root_path)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SurveyWeb::Application.routes.draw do
   scope "(:locale)", :locale => /#{I18n.available_locales.join('|')}/ do
+
+    match '/auth/:provider/callback', :to => 'sessions#create'
+    match '/signout', :to => 'sessions#destroy', :as => 'signout'
+
     resources :surveys do
       resources :responses, :only => [:new, :create, :show]
     end
+
     root :to => 'surveys#index'
   end
 end
