627db967078cf5689da29ca330aab15441f3bedb
Nivedita <nivedita@c42.in>
2012-08-23 18:11:27 +0530

    Added mandatory, image and max_length fields to Question model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120817104747) do
+ActiveRecord::Schema.define(:version => 20120823105939) do
 
   create_table "answers", :force => true do |t|
     t.text     "content"
@@ -26,8 +26,11 @@ ActiveRecord::Schema.define(:version => 20120817104747) do
   create_table "questions", :force => true do |t|
     t.text     "content"
     t.integer  "survey_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.boolean  "mandatory",  :default => false
+    t.string   "image"
+    t.integer  "max_length"
   end
 
   add_index "questions", ["survey_id"], :name => "index_questions_on_survey_id"
