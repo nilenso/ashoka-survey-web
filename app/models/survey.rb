b3e66e508136bfe42ced4bd5824a9a53013791ff
Nivedita <nivedita@c42.in>
2012-09-04 15:00:30 +0530

    Expiry date validated to not be in past for a survey

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,7 +3,16 @@
 class Survey < ActiveRecord::Base
   attr_accessible :name, :expiry_date, :description, :questions_attributes
   validates_presence_of :name, :expiry_date
+  validate :expiry_date_should_not_be_in_past
   has_many :questions, :dependent => :destroy
   has_many :responses, :dependent => :destroy
   accepts_nested_attributes_for :questions
+
+  private
+
+  def expiry_date_should_not_be_in_past
+		if !expiry_date.blank? and expiry_date < Date.today
+		 	errors.add(:expiry_date, "can't be in the past")
+		end
+  end
 end
