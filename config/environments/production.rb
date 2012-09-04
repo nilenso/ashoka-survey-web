3fd78962c48c38ce53c7393c4985a12773d92688
Nivedita <nivedita@c42.in>
2012-09-04 12:57:39 +0530

    Precompiling the css for backbone survey builder

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ SurveyWeb::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( survey_builder.css surveys_table.css )
+  config.assets.precompile += %w( survey_builder.css surveys_table.css backbone_survey_builder.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
