99e3ebff134c8e3d737230e350277fea450abd1f
Srihari Sriraman <srihari@c42.in>
2012-09-06 17:07:30 +0530

    Ensured uniqueness of order number within a survey

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,14 +2,14 @@
 
 class Question < ActiveRecord::Base
   belongs_to :survey
-  attr_accessible :content, :mandatory, :max_length, :image, :type, :survey_id, :max_value, :min_value
-  validates_presence_of :content
+  attr_accessible :content, :mandatory, :max_length, :image, :type, :survey_id, :max_value, :min_value, :order_number
+  validates_presence_of :content, :order_number
   has_many :answers, :dependent => :destroy
   has_many :options, :dependent => :destroy
   has_attached_file :image, :styles => { :medium => "300x300>" }
   accepts_nested_attributes_for :options
   validate :min_value_less_than_max_value
-
+  validates_uniqueness_of :order_number, :scope => :survey_id
   private
 
   def min_value_less_than_max_value
