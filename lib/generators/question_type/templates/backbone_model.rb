6b8b90d11a75d0fa4fce5c954abe0d9a03052eee
Smit Shah <who828@gmail.com>
2012-09-07 15:18:34 +0530

    Refactored the generator

--- lib/generators/question_type/templates/backbone_model.rb
+++ lib/generators/question_type/templates/backbone_model.rb
@@ -1,8 +1,10 @@
 # Job of the class goes here
 
-class SurveyBuilder.Models.<%= class_name %>Model extends Backbone.RelationalModel
+class SurveyBuilder.Models.<%= class_name %>Model extends SurveyBuilder.Models.QuestionModel
   defaults: {
     type: '<%= class_name %>',
     content: 'Untitled question'
     mandatory: false
   }
+
+SurveyBuilder.Models.<%= class_name %>QuestionModel.setup()
