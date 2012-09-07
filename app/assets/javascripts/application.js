nilenso/ashoka-survey-web

commit 8b4e862447749bd28ff96e0cb2f74e7ab0e7fc52

Removed old survey builder and all related assets

app/assets/javascripts/application.js
@@ -22,5 +22,3 @@
 //= require backbone_datalink
 //= require backbone/survey_builder
 //= require_tree .
-
-var SurveyApp = {};

app/assets/javascripts/survey_row.js.coffee
@@ -1,18 +1,0 @@
-class SurveyRow
-  constructor: (@description_cell) ->
-    @hideDetailedDescription()
-    @description_cell.find(".more_description_link").click(@toggle_description)
-    @description_cell.find(".less_description_link").click(@toggle_description)
-
-  hideDetailedDescription: ->
-    @description_cell.find('.more_description').hide()
-    @description_cell.find('.less_description_link').hide()
-
-  toggle_description: =>
-    @description_cell.find('.more_description').toggle();
-    @description_cell.find('.truncated_description').toggle();
-    @description_cell.find('.more_description_link').toggle();
-    @description_cell.find('.less_description_link').toggle();
-
-
-SurveyApp.SurveyRow = SurveyRow

app/assets/javascripts/survey_builder.js.coffee
@@ -1,34 +1,0 @@
-# Collection of SurveyElements
-
-class SurveyBuilder
-
-  constructor: (@sidebar_div, @dummy_div, @question_count) ->
-    @sidebar_div.find(".add_question_field").click(@add_new_question)
-    @elements = []
-
-  add_new_question: =>
-    type = $(event.target).data('type')
-    actual = $(Mustache.render(@sidebar_div.find("##{type}_question_template").html(), id: @question_count))
-    dummy = $(Mustache.render(@sidebar_div.find("##{type}_dummy_question_template").html(), id: @question_count++))
-    @sidebar_div.find('#questions').append(actual)
-    @sidebar_div.find('#questions').find('fieldset').hide()
-    @dummy_div.find('#dummy_questions').append(dummy)
-    @create_element(actual,dummy)
-
-  show_element: (event) =>
-    @hide_all_elements()
-    event.data.element.show()
-    @click_settings_tab()
-
-  hide_all_elements: =>
-    element.hide() for element in @elements
-
-  create_element: (actual, dummy) =>
-    element = new SurveyApp.SurveyElement(actual, dummy);
-    @elements.push(element)
-    dummy.bind('click', { element: element }, @show_element)
-
-  click_settings_tab: =>
-    $(".tabs li").last().click()
-
-SurveyApp.SurveyBuilder = SurveyBuilder

app/assets/javascripts/survey_element.js.coffee
@@ -1,33 +1,0 @@
-# Binding between the actual and dummy fieldsets
-
-class SurveyElement
-  constructor: (@actual, @dummy) ->
-    @actual.find('*').bind('keyup change', @mirror)
-    @actual.find("*[type='checkbox']").bind('click', @toggle_mandatory)
-
-  mirror: (event) =>
-    name = $(event.target).attr('name')
-
-    dummy_val = $(event.target).val()
-    if dummy_val == ""
-      dummy_val = @dummy.find("*[name=\"#{name}\"]").attr('default_value')
-
-    @dummy.find("*[name=\"#{name}\"]").val(dummy_val);
-    @dummy.find("*[name=\"#{name}\"]").text(dummy_val);
-
-  toggle_mandatory: (event) =>
-    @dummy.find('abbr').toggle()
-
-  show: (event) =>
-    @actual.show()
-    if @dummy.attr('id') == "dummy_survey_details"
-      @dummy.addClass("details_active")
-    else
-      @dummy.addClass("active")    
-
-  hide: =>
-    @actual.hide()
-    @dummy.removeClass("active")
-    @dummy.removeClass("details_active")
-
-SurveyApp.SurveyElement = SurveyElement

app/assets/javascripts/tabs.js.coffee
@@ -1,17 +1,0 @@
-SurveyApp.tabify = (tabsContainer) ->
-  tabsContainer.find('.tabs li').click(@switch_tabs) 
-
-SurveyApp.switch_tabs = (event) ->
-  clicked_tab = $(event.target)
-  container = clicked_tab.parent().parent()
-
-  all_tabs = container.find('li')
-  all_tabs.removeClass('active')
-
-  clicked_tab.addClass('active')
-
-  all_panes = container.children('div')
-  all_panes.hide()
-
-  target_pane = container.find(clicked_tab.data('tab-target'))
-  target_pane.show()

app/assets/stylesheets/application.css
@@ -11,6 +11,5 @@
  *= require_self
  *= require base_layout
  *= require jquery_ui
- *= require my_formtastic_changes
  *= require formtastic
  */

app/assets/stylesheets/survey_builder.css.scss
@@ -1,120 +1,0 @@
-@import 'susy';
-@import 'base_layout';
-@import "compass/css3/border-radius";
-
-$total-columns: 12;
-$column-width: 60px;
-$gutter-width: 20px;
-$grid-padding: 10px;
-
-$app_min_height: 600px;
-
-.container{
-  @include container;
-}
-
-.dummy_form_display {
-  @include span-columns(7 omega, 12);
-  margin-bottom: 1em;
-  border: 1px solid $border_color;
-  padding: 1em;
-  height: $app_min_height;
-  overflow: auto;
-
-  fieldset {
-    padding: 4px;
-    &:hover {
-      color: $link_hover_color;
-      border: 1px dashed $border_color;
-      @include border-radius(4px, 4px);
-    }
-  }
-
-  .active {
-    padding: 4px;
-    background: $question_bg_color;
-    @include box-sizing(content-box);
-    @include border-radius(4px, 4px);
-  }
-
-  .details_active {
-    padding: 4px;
-    background: $survey_details_bg_color;
-    @include box-sizing(content-box);
-    @include border-radius(4px, 4px);
-  }
-
-  label {
-    display :block;
-  }
-
-  #dummy_questions {
-    input {
-      width: 98%;
-    }
-    abbr {
-      color: $error_color;
-    }
-  }
-
-  .inline_error {
-    color: $error_color;
-  }
-}
-
-.sidebar { @include span-columns(4, 12); }
-
-.question_picker_pane {
-  border: 1px solid $border_color;
-  padding: 1em;
-}
-
-.settings_pane{
-  border: 1px solid $border_color;
-  padding: 1em;
-  display: none;
-
-  #survey_details {
-    padding: 4px;
-    background: $survey_details_bg_color;
-    @include box-sizing(content-box);
-    @include border-radius(4px, 4px);
-  }
-
-  #questions {
-    padding: 4px;
-    background: $question_bg_color;
-    @include box-sizing(content-box);
-    @include border-radius(4px, 4px);
-    @include clearfix;
-
-    input#foo{
-      display: block !important;
-    }
-  }
-}
-
-.tabs {
-  li {
-    @include border-top-left-radius(0.5em);
-    @include border-top-right-radius(0.5em);
-    color: $tab_header_color;
-    border: 1px solid $border_color;
-    border-bottom: 0;
-    padding: 0.2em 0.6em 0 0.6em;
-    font-family: sans-serif;
-    font-size: 0.9em;
-    display: inline;
-    cursor: pointer;
-    a {
-      text-decoration: none;
-      color: $link_color;
-      &:hover {
-        color: $link_hover_color;
-      }
-    }
-  }
-  .active {
-    background-color: $active_tab_color;
-  }
-}

app/controllers/surveys_controller.rb
@@ -4,21 +4,6 @@ class SurveysController < ApplicationController
     @surveys = Survey.paginate(:page => params[:page], :per_page => 10)
   end
 
-  def new
-    @survey = Survey.new
-  end
-
-  def create
-    @survey = Survey.new(params[:survey])
-
-    if @survey.save
-      redirect_to root_path
-      flash[:notice] = t "flash.survey_created"
-    else
-      render :new
-    end
-  end
-
   def destroy
     survey = Survey.find(params[:id])
     survey.destroy

commit 15ad89f9252df423a219cda3115252560ba47f9c

Fix broken surveyRow (more/less) broken in 8b4e862447749bd28ff96e0cb2f74e7ab0e7fc52.

app/assets/javascripts/application.js
@@ -26,3 +26,5 @@
 //= require backbone_datalink
 //= require backbone/survey_builder
 //= require_tree .
+
+var SurveyApp = {};

app/assets/javascripts/survey_row.js.coffee
@@ -1,0 +1,18 @@
+class SurveyRow
+  constructor: (@description_cell) ->
+    @hideDetailedDescription()
+    @description_cell.find(".more_description_link").click(@toggle_description)
+    @description_cell.find(".less_description_link").click(@toggle_description)
+
+  hideDetailedDescription: ->
+    @description_cell.find('.more_description').hide()
+    @description_cell.find('.less_description_link').hide()
+
+  toggle_description: =>
+    @description_cell.find('.more_description').toggle();
+    @description_cell.find('.truncated_description').toggle();
+    @description_cell.find('.more_description_link').toggle();
+    @description_cell.find('.less_description_link').toggle();
+
+
+SurveyApp.SurveyRow = SurveyRow