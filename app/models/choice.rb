abd1148073a95d76e23fb672c03addccd1a34eeb
Timothy Andrew <mail@timothyandrew.net>
2012-09-11 13:50:23 +0530

    A choice stores an option_id instead of content

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,5 +1,5 @@
 class Choice < ActiveRecord::Base
   belongs_to :answer
+  belongs_to :option
   attr_accessible :content
-  validates_presence_of :content
 end
--- spec/models/choice_spec.rb
+++ spec/models/choice_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Choice do
-  it { should respond_to :content }
+  it { should belong_to :option }
   it { should belong_to :answer }
-  it { should validate_presence_of :content }
 end
