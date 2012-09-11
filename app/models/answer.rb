29debc3ba6ebfe449ee58b330ac53fe9b67e4dad
Srihari Sriraman <srihari@c42.in>
2012-09-11 16:31:49 +0530

    Added validation for minimum and maximum values
    In numeric question type

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -33,8 +33,10 @@ class Answer < ActiveRecord::Base
 
   def content_should_be_in_range
     min_value, max_value = question.min_value, question.max_value
-    if min_value && max_value && (min_value..max_value).exclude?(content.to_i)
-      errors.add(:content, I18n.t("answers.validations.exceeded_range"))
+    if min_value && content.to_i < min_value
+      errors.add(:content, I18n.t("answers.validations.exceeded_lower_limit"))
+    elsif max_value && content.to_i > max_value 
+      errors.add(:content, I18n.t("answers.validations.exceeded_higher_limit"))
     end
   end
 
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -23,24 +23,35 @@ describe Answer do
       answer.should_not be_valid
     end
 
-    it "does not save if the answer is not within the range of a numeric question" do
-      question = FactoryGirl.create(:question, :type => 'NumericQuestion', :min_value => 5, :max_value => 7)
+    it "does not save if the answer is less than the minimum value" do
+      question = FactoryGirl.create(:question, :type => 'NumericQuestion', :min_value => 5)
       answer = FactoryGirl.build(:answer, :question_id => question.id)
       question.answers << answer
 
       answer.content = 3
       answer.should_not be_valid
     end
+    
+    context "when validating numeric questions"
+      it "does not save if the answer is greater than the maximum value" do
+        question = FactoryGirl.create(:question, :type => 'NumericQuestion', :max_value => 5)
+        answer = FactoryGirl.build(:answer, :question_id => question.id)
+        question.answers << answer
 
-    it "does not save if the answer to a date type question is not a valid date" do
-      question = FactoryGirl.create(:question, :type => 'DateQuestion')
-      answer = FactoryGirl.build(:answer, :question_id => question.id)
-      question.answers << answer
+        answer.content = 8
+        answer.should_not be_valid
+      end
 
-      answer.content = "4235643861"
-      answer.should_not be_valid
-      answer.content = "1990/10/24"
-      answer.should be_valid
+      it "does not save if the answer to a date type question is not a valid date" do
+        question = FactoryGirl.create(:question, :type => 'DateQuestion')
+        answer = FactoryGirl.build(:answer, :question_id => question.id)
+        question.answers << answer
+
+        answer.content = "4235643861"
+        answer.should_not be_valid
+        answer.content = "1990/10/24"
+        answer.should be_valid
+      end
     end
 
     context "for multi-choice questions" do
