d43c6907bf6bbd0ad651f95a74dd22db104fb5b0
Nivedita <nivedita@c42.in>
2012-09-25 17:07:51 +0530

    Fixed photo question response when not mandatory

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -22,13 +22,17 @@ class Answer < ActiveRecord::Base
     ids.each { |option_id| choices.new(:option_id => option_id) }
   end
 
+  def text_type?
+    question.type != "MultiChoiceQuestion" && question.type != "PhotoQuestion"
+  end
+
   private
 
   def maximum_photo_size
     if question.type == "PhotoQuestion"
       if question.max_length && question.max_length.megabytes < photo_file_size
         errors.add(:photo, I18n.t('answers.validations.exceeds_maximum_size'))
-      elsif 5.megabytes < photo_file_size
+      elsif photo_file_size && 5.megabytes < photo_file_size
         errors.add(:photo, I18n.t('answers.validations.exceeds_maximum_size'))
       end
     end
