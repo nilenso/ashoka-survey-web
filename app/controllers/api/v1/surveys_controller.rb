b06edac872190469bc4d7bc41bfdfd865b0da246
Timothy Andrew <mail@timothyandrew.net>
2012-09-26 18:03:05 +0530

    move the api/mobile endpoint to api/

---
+++ app/controllers/api/v1/surveys_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module V1
+    class SurveysController < ApplicationController
+      def index
+        render :json => Survey.select("id, name, description, expiry_date")
+      end
+
+      def show
+        survey = Survey.find(params[:id])
+        render :json => survey.questions.select("id, content")
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,13 +18,8 @@ SurveyWeb::Application.routes.draw do
     scope :module => :v1 do
       resources :questions, :except => [:edit, :new]
       resources :options, :except => [:edit, :new, :show]
+      resources :surveys, :only => [:index, :show]
       post 'questions/:id/image_upload' => 'questions#image_upload'
-    end
-
-    namespace :mobile, :defaults => { :format => 'json'} do
-      scope :module => :v1 do
-        resources :surveys, :only => [:index, :show]
-      end
     end
   end
 end
