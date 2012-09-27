aa47b9e289507ed7cce8bb10bcc01b3216621495
Timothy Andrew <mail@timothyandrew.net>
2012-09-27 22:48:34 +0530

    In the SurveyController#index page, a normal user can only see the surveys that are shared with him.
    Implemented this logic in the Cancan abilites file.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -16,8 +16,10 @@ class Ability
           survey.organization_id == user_info[:org_id]
         end
       elsif role == 'user'
-        can :read, Survey, ["organization_id = ?", user_info[:org_id]] do |survey|
-          survey.organization_id == user_info[:org_id]
+        can :read, Survey, 
+        Survey.joins(:survey_users).where('survey_users.user_id' => user_info[:user_id])
+        .where('surveys.organization_id' => user_info[:org_id]) do |survey|
+          SurveyUser.find_by_user_id_and_survey_id(user_info[:user_id], survey.id)
         end
       end
     end
