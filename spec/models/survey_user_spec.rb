139a92b56f1a003a158ba39cf9c1e5287bbaa64d
Timothy Andrew <mail@timothyandrew.net>
2012-09-27 15:26:18 +0530

    Correct misnamed variable in survey_user_spec
    Survey_user, not survey.

--- spec/models/survey_user_spec.rb
+++ spec/models/survey_user_spec.rb
@@ -9,9 +9,9 @@ describe SurveyUser do
 
   it "validates uniqueness of the (survey_id, user_id) tuple" do
     SurveyUser.create(:survey_id => 5, :user_id => 5)
-    survey = SurveyUser.new(:survey_id => 5, :user_id => 5)
-    survey.should_not be_valid
-    survey = SurveyUser.new(:survey_id => 5, :user_id => 6)
-    survey.should be_valid
+    survey_user = SurveyUser.new(:survey_id => 5, :user_id => 5)
+    survey_user.should_not be_valid
+    survey_user = SurveyUser.new(:survey_id => 5, :user_id => 6)
+    survey_user.should be_valid
   end
 end
