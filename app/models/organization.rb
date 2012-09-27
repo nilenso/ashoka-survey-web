e7220dc29ff5e319623d834868914d1d79cc425b
Srihari Sriraman <srihari@c42.in>
2012-09-27 10:42:25 +0530

    Removed sync method from Organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,15 +3,4 @@ class Organization < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
   has_many :surveys
-
-  def self.sync(organizations)
-    organizations.each do |organization|
-      unless exists?(organization['id'])
-        org = Organization.new
-        org.id = organization['id']
-        org.name = organization['name']
-        org.save
-      end
-    end
-  end
 end
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -6,9 +6,4 @@ describe Organization do
   it {should validate_presence_of :name}
   it {should validate_uniqueness_of :name}
   it {should have_many :surveys}
-
-  it "stores organizations if they do not exist" do
-    Organization.sync([{'id' => '1', 'name' => 'orgname'}])
-    Organization.find(1).should_not be_nil
-  end
 end
