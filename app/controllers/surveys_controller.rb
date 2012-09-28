f7e11aa510da5eed848436cfe804c02831000e66
Timothy Andrew <mail@timothyandrew.net>
2012-09-28 11:21:20 +0530

    Migrate SurveysController#create to CanCan.

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,9 +1,9 @@
 require 'will_paginate/array'
 
 class SurveysController < ApplicationController
-  load_and_authorize_resource :only => [:index, :destroy, :new]
+  load_and_authorize_resource :only => [:index, :destroy, :new, :create]
 
-  before_filter :require_cso_admin, :except => [:new, :destroy, :index, :build]
+  before_filter :require_cso_admin, :except => [:create, :new, :destroy, :index, :build]
   before_filter :survey_unpublished, :only => [:build]
 
   def index
