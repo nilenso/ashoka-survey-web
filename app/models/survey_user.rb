226989e1145bfce84ae1d289d07c4e44d0a50e6c
Timothy Andrew <mail@timothyandrew.net>
2012-09-28 16:18:22 +0530

    use :scope directly instead of :uniqueness => {:scope}

--- app/models/survey_user.rb
+++ app/models/survey_user.rb
@@ -1,5 +1,5 @@
 class SurveyUser < ActiveRecord::Base
   attr_accessible :survey_id, :user_id
   belongs_to :survey
-  validates_uniqueness_of :user_id, :uniqueness => { :scope => :survey_id }
+  validates_uniqueness_of :user_id, :scope => :survey_id 
 end
