941be8b3c8c537f758a6b1f22d849e61212bd6c9
Srihari Sriraman <srihari@c42.in>
2012-10-08 12:49:24 +0530

    Removed generic Question specs from PhotoQuestion

--- spec/models/photo_question_spec.rb
+++ spec/models/photo_question_spec.rb
@@ -1,13 +1,7 @@
 require 'spec_helper'
 
 describe PhotoQuestion do
-  it { should respond_to :content }
-  it { should respond_to :mandatory }
-  it { should respond_to :image }
   it { should respond_to :max_length }
-  it { should belong_to :survey }
-  it { should have_many(:answers).dependent(:destroy) }
-  it { should validate_presence_of :content }
 
   it "is a question with type = 'PhotoQuestion'" do
     PhotoQuestion.create(:content => "hello")
