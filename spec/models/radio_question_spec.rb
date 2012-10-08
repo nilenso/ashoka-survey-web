1b69310458972d6e22846b792663293b7bbfded4
Srihari Sriraman <srihari@c42.in>
2012-10-08 12:49:58 +0530

    Removed generic Question specs from RadioQuestion

--- spec/models/radio_question_spec.rb
+++ spec/models/radio_question_spec.rb
@@ -1,13 +1,7 @@
 require 'spec_helper'
 
 describe RadioQuestion do
-  it { should respond_to :content }
-  it { should belong_to :survey }
-  it { should have_many(:answers).dependent(:destroy) }
   it { should have_many(:options).dependent(:destroy) }
-  it { should validate_presence_of :content }
-  it { should respond_to :mandatory }
-  it { should respond_to :image }
   it { should accept_nested_attributes_for(:options) }
 
   it "is a question with type = 'RadioQuestion'" do
