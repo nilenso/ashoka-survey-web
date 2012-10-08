997e0028ddad98db2f6194836f7d42d46acf1fdf
Srihari Sriraman <srihari@c42.in>
2012-10-08 12:47:39 +0530

    Removed generic Question specs from MultichoiceQuestion

--- spec/models/multi_choice_question_spec.rb
+++ spec/models/multi_choice_question_spec.rb
@@ -1,13 +1,6 @@
 require 'spec_helper'
 
 describe MultiChoiceQuestion do
-  it { should respond_to :content }
-  it { should respond_to :mandatory }
-  it { should respond_to :image }
-  it { should respond_to :max_length }
-  it { should belong_to :survey }
-  it { should have_many(:answers).dependent(:destroy) }
-  it { should validate_presence_of :content }
   
   it "is a question with type = 'MultiChoiceQuestion'" do
     MultiChoiceQuestion.create(:content => "hello")
