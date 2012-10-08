d43bc2f9466bcdfbc69011305ffa063f73d4c0b4
Srihari Sriraman <srihari@c42.in>
2012-10-08 12:42:38 +0530

    Removed generic Question specs from NumericQuestion

--- spec/models/numeric_question_spec.rb
+++ spec/models/numeric_question_spec.rb
@@ -1,14 +1,8 @@
 require 'spec_helper'
 
 describe NumericQuestion do
-  it { should respond_to :content }
-  it { should respond_to :mandatory }
-  it { should respond_to :image }
   it { should respond_to :max_value }
   it { should respond_to :min_value }
-  it { should belong_to :survey }
-  it { should have_many(:answers).dependent(:destroy) }
-  it { should validate_presence_of :content }
   
   it "is a question with type = 'NumericQuestion'" do
     NumericQuestion.create(:content => "hello",:order_number => 11)
