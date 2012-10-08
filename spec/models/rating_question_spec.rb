23f98debd1a3fe42563a1ff53f8eec36786dd476
Srihari Sriraman <srihari@c42.in>
2012-10-08 12:50:18 +0530

    Removed generic Question specs from RatingQuestion

--- spec/models/rating_question_spec.rb
+++ spec/models/rating_question_spec.rb
@@ -1,13 +1,7 @@
 require 'spec_helper'
 
 describe RatingQuestion do
-  it { should respond_to :content }
-  it { should respond_to :mandatory }
-  it { should respond_to :image }
   it { should respond_to :max_length }
-  it { should belong_to :survey }
-  it { should have_many(:answers).dependent(:destroy) }
-  it { should validate_presence_of :content }
   
   it "is a question with type = 'RatingQuestion'" do
     RatingQuestion.create(:content => "hello")
