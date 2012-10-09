1dba9734100efc174225919e418033746ab48da5
Nivedita <nivedita@c42.in>
2012-10-09 15:24:17 +0530

    Refactored the adding a response flow.

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -3,27 +3,20 @@ class ResponsesController < ApplicationController
   load_and_authorize_resource :through => :survey
 
   before_filter :survey_published
-  
-  def new
-    @survey = Survey.find(params[:survey_id])
-    @response = ResponseDecorator.new(Response.new)
-    @survey.questions.each { |question| @response.answers << Answer.new(:question_id => question.id) }
-  end
 
   def index
     @responses = @responses.paginate(:page => params[:page], :per_page => 10)
   end
 
   def create
-    @response = ResponseDecorator.new(Response.new(params[:response]))
-    @response.survey = Survey.find(params[:survey_id])
-    @response.user_id = current_user
-    @response.organization_id = current_user_org
-    if @response.save
-      redirect_to root_path, :notice => t("responses.new.response_saved")
-    else
-      render :new
-    end
+    response = ResponseDecorator.new(Response.new)
+    survey = Survey.find(params[:survey_id])
+    response.survey = survey
+    response.user_id = current_user
+    response.organization_id = current_user_org
+    survey.questions.each { |question| response.answers << Answer.new(:question_id => question.id) }
+    response.save(:validate => false)
+    redirect_to edit_survey_response_path(:id => response.id, :survey_id => survey.id)
   end
 
   def edit
@@ -32,8 +25,8 @@ class ResponsesController < ApplicationController
   end
 
   def update
-    response = Response.find(params[:id])
-    if response.update_attributes(params[:response])
+    @response = ResponseDecorator.find(params[:id])
+    if @response.update_attributes(params[:response])
       redirect_to survey_responses_path, :notice => "Successfully updated"
     else
       flash[:error] = "Error"
