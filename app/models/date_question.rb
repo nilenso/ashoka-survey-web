72579c25904089057c6876dece4c608301a1e1b2
Smit Shah <who828@gmail.com>
2012-10-24 18:31:01 +0530

    Added reports for date questions

--- app/models/date_question.rb
+++ app/models/date_question.rb
@@ -1,4 +1,10 @@
 # A question with a date type answer
 
 class DateQuestion < Question
+  def report_data
+    answers_content = answers.map(&:content)
+    answers_content.uniq.inject([]) do |data, content|
+      data.push [content, answers_content.count(content)]
+    end
+  end
 end
--- spec/models/date_question_spec.rb
+++ spec/models/date_question_spec.rb
@@ -10,4 +10,11 @@ describe DateQuestion do
   end
 
   it_behaves_like "a question"
+
+  it "generates report data" do
+    date_question = DateQuestion.create!(:content => "some date question")
+    date_question.answers << FactoryGirl.create( :answer_with_complete_response, :content=>"2010/02/21")
+    date_question.answers << FactoryGirl.create( :answer_with_complete_response, :content=>"2012/01/11")
+    date_question.report_data.should == [["2012/01/11", 1], ["2010/02/21", 1]]
+  end
 end
