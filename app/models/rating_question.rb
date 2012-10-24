20ed0198b986f45b9a0a9522986068c718a2266a
Smit Shah <who828@gmail.com>
2012-10-24 16:35:21 +0530

    Added reports for Rating Questions

--- app/models/rating_question.rb
+++ app/models/rating_question.rb
@@ -3,4 +3,19 @@
 class RatingQuestion < Question
   attr_accessible :max_length
   validates_numericality_of :max_length, :only => :integer, :greater_than => 0, :allow_nil => true
+
+  def report_data
+    answers_content = answers.map(&:content)
+    answers_content.uniq.inject([]) do |data, content|
+      data.push [content.to_i, answers_content.count(content)]
+    end
+  end
+
+  def min_value_for_report
+    0
+  end
+
+  def max_value_for_report
+    max_length
+  end
 end
--- spec/models/rating_question_spec.rb
+++ spec/models/rating_question_spec.rb
@@ -13,4 +13,24 @@ describe RatingQuestion do
   it_behaves_like "a question"
 
   it_behaves_like "a question with max length"
+
+  context "for report data" do
+    let (:rating_question) { RatingQuestion.new(:content => "foo", :max_length => 10) }
+
+    it "generates report data" do
+      5.times { rating_question.answers << FactoryGirl.create(:answer_with_complete_response, :content=>'2') }
+      3.times { rating_question.answers << FactoryGirl.create(:answer_with_complete_response, :content=>'5') }
+      9.times { rating_question.answers << FactoryGirl.create(:answer_with_complete_response, :content=>'9') }
+      rating_question.save
+      rating_question.report_data.should == [[2, 5],[5, 3], [9, 9]]
+    end
+
+    it "gives 0 for min_value for reports" do
+      rating_question.min_value_for_report.should == 0
+    end
+
+    it "gives max_value for miax_value for reports" do
+      rating_question.max_value_for_report.should == rating_question.max_length
+    end
+  end
 end
