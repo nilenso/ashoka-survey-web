032c270f6133c8bbee1df8be1666823c8fd8c4f2
Smit Shah <who828@gmail.com>
2012-10-24 12:35:30 +0530

    Added a choices method to MultiChoiceQuestion

--- app/models/multi_choice_question.rb
+++ app/models/multi_choice_question.rb
@@ -4,7 +4,14 @@ class MultiChoiceQuestion < Question
   has_many :options, :dependent => :destroy,  :foreign_key => :question_id
 
   def report_data
-    choice_ids = Choice.joins(:answer => :response).where(:responses => {:status => 'complete'}, :option_id => options.map(&:id)).map(&:option_id)
+    return [] if choices.blank?
+    choice_ids = choices.map(&:option_id)
     options.map { |option| [option.content, choice_ids.count(option.id)] }
+  end
+
+  private
+
+  def choices
+    Choice.joins(:answer => :response).where(:responses => {:status => 'complete'}, :option_id => options.map(&:id))
   end
 end
