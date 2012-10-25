70865a3ad18af4003464f7a7eb93323ec6cf8987
Srihari Sriraman <srihari@c42.in>
2012-10-25 17:58:03 +0530

    Changed the Responses API controller for mobile
    Incomplete responses are sent back. Complete ones arent.
    Any invalid response is sent back with a bad_request.

--- app/controllers/api/v1/responses_controller.rb
+++ app/controllers/api/v1/responses_controller.rb
@@ -4,16 +4,19 @@ module Api
       def create
         response = Response.new
         response.user_id = response.organization_id = 0 # temporary fix for no login on mobile
-        answers_attributes = params[:response].delete(:answers_attributes)        
+        answers_attributes = params[:response].delete(:answers_attributes)
         response.update_attributes(params[:response]) # Response isn't created before the answers, so we need to create the answers after this.
+        response.validating if params[:response][:status] == "complete"
         response.update_attributes({:answers_attributes => answers_attributes}) if response.save
-        response.validating
-        if response.valid?
+
+        if response.incomplete? && response.valid?
           render :json => response.to_json
+        elsif response.validating? && response.valid?
           response.complete
+          render :nothing => true
         else
           response.incomplete
-          render :nothing => true, :status => :bad_request
+          render :json => response.to_json, :status => :bad_request
         end
       end
 
--- spec/controllers/api/v1/responses_controller_spec.rb
+++ spec/controllers/api/v1/responses_controller_spec.rb
@@ -3,10 +3,12 @@ require 'spec_helper'
 module Api
   module V1
     describe ResponsesController do
-     
+
       context "POST 'create'" do
+        let (:survey) { FactoryGirl.create(:survey) }
+        let (:question) { FactoryGirl.create(:question) }
+
         it "creates an response" do
-          survey = FactoryGirl.create(:survey)
           resp = FactoryGirl.attributes_for(:response)
           expect {
             post :create, :survey_id => survey.id, :response => resp
@@ -14,25 +16,36 @@ module Api
         end
 
         it "creates the nested answers" do
-          survey = FactoryGirl.create(:survey)
-          question = FactoryGirl.create(:question)
           resp = FactoryGirl.attributes_for(:response, :survey_id => survey.id, :answers_attributes =>  { '0' => {'content' => 'asdasd', 'question_id' => question.id} })
           expect {
             post :create, :survey_id => survey.id, :response => resp
           }.to change { Answer.count }.by 1
         end
 
-        it "should return the newly created response as JSON" do
-          survey = FactoryGirl.create(:survey)
-          question = FactoryGirl.create(:question)
+        it "should return the newly created response as JSON if it is incomplete" do
           resp = FactoryGirl.attributes_for(:response, :survey_id => survey.id, :answers_attributes =>  { '0' => {'content' => 'asdasd', 'question_id' => question.id} })
           post :create, :response => resp
           response.should be_ok
           JSON.parse(response.body).keys.should =~ Response.new.attributes.keys
         end
 
-        it "returns a bad request if you give a invalid response" do
-          question = FactoryGirl.create(:question)
+        it "should not return the newly created response if it is complete" do
+          resp = FactoryGirl.attributes_for(:response, :survey_id => survey.id, :status => 'complete', :answers_attributes =>  { '0' => {'content' => 'asdasd', 'question_id' => question.id} })
+          post :create, :response => resp
+          response.should be_ok
+          response.body.should be_blank
+        end
+
+        it "should mark a complete response as incomplete and return it if it fails the validation" do
+          question = FactoryGirl.create(:question, :type => 'SingleLineQuestion', :mandatory => true)
+          resp = FactoryGirl.attributes_for(:response, :survey_id => survey.id, :status => 'complete', :answers_attributes =>  { '0' => {'content' => "", 'question_id' => question.id} })
+          post :create, :response => resp
+          response.should_not be_ok
+          response.status.should == 400
+          JSON.parse(response.body).keys.should =~ Response.new.attributes.keys
+        end
+
+        it "returns the response with a bad_request if you give a invalid response" do
           resp = FactoryGirl.attributes_for(:response, :answers_attributes =>  { '0' => {'content' => 'asdasd', 'question_id' => question.id} })
           post :create, :response => resp
           response.should_not be_ok
