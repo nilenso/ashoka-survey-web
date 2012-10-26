384b048014149ac3c9b5c062c40b8f6d8c9c7133
Srihari Sriraman <srihari@c42.in>
2012-10-26 16:37:48 +0530

    Updated update as apt to handle response statuses

--- app/controllers/api/v1/responses_controller.rb
+++ app/controllers/api/v1/responses_controller.rb
@@ -23,17 +23,22 @@ module Api
 
       def update
         response = Response.find(params[:id])
+        response.user_id = response.organization_id = 0 # temporary fix for no login on mobile
+        answers_attributes = params[:response].delete(:answers_attributes)
+        response.update_attributes(params[:response]) # Response isn't saved before the answers, so we need to create the answers after this.
+        response.validating if params[:response][:status] == "complete"
+        response.update_attributes({:answers_attributes => answers_attributes}) if response.save
         # TODO
-        # validate as appropriate
         # merge response and its answers based on update time in model
-        # follow same logic as create for giving back response with answers
-        response.validating
-        if response.update_attributes(params[:response])
+        if response.incomplete? && response.valid?
+          render :json => response.to_json(:methods => :answers)
+        elsif response.validating? && response.valid?
           response.complete
-          render :json => response.to_json
+          render :nothing => true
         else
-          response.complete
-          render :nothing => true, :status => :bad_request
+          response_json = response.to_json(:methods => :answers)
+          response.destroy
+          render :json => response_json, :status => :bad_request
         end
       end
     end
