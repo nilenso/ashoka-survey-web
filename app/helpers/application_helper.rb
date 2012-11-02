54c1c62f142be538a9678a9e90d709d947f58b45
Timothy Andrew <mail@timothyandrew.net>
2012-11-02 11:54:35 +0530

    Move #icon_for to ApplicationHelper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def server_url
     request.protocol + request.host_with_port
   end
+
+  def icon_for(css_class, text)
+    "<i class=#{css_class}></i>".html_safe + text
+  end
 end
--- app/helpers/survey_helper.rb
+++
@@ -1,5 +0,0 @@
-module SurveyHelper
-  def icon_for(css_class, text)
-    "<i class=#{css_class}></i>".html_safe + text
-  end
-end
