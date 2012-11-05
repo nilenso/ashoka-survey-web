d4d3cb3de9eef147f6b93a75fd8c834d87148603
Smit Shah <who828@gmail.com>
2012-11-05 16:39:16 +0530

    Added precompile on initialize

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ SurveyWeb::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.assets.initialize_on_precompile = true
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
