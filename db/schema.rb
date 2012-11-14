6ee2ff7cfd4e253d2a4a1cfefa9d09942ca7560f
Srihari Sriraman <srihari@c42.in>
2012-11-14 11:44:06 +0530

    Added auth key to surveys

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121112191031) do
+ActiveRecord::Schema.define(:version => 20121114061152) do
 
   create_table "answers", :force => true do |t|
     t.text     "content"
@@ -46,6 +46,12 @@ ActiveRecord::Schema.define(:version => 20121112191031) do
   end
 
   add_index "options", ["question_id"], :name => "index_options_on_question_id"
+
+  create_table "organizations", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "participating_organizations", :force => true do |t|
     t.integer  "survey_id"
@@ -104,6 +110,7 @@ ActiveRecord::Schema.define(:version => 20121112191031) do
     t.boolean  "published",       :default => false
     t.integer  "organization_id"
     t.boolean  "public",          :default => false
+    t.string   "auth_key"
   end
 
   add_index "surveys", ["organization_id"], :name => "index_surveys_on_organization_id"
