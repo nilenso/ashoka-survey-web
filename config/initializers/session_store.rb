a85c7816b0cca5ca1a35f158d3a9059548d86977
Nivedita <nivedita@c42.in>
2012-11-23 17:11:07 +0530

    Made cookies to not be http only

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-SurveyWeb::Application.config.session_store :cookie_store, key: '_survey-web_session'
+SurveyWeb::Application.config.session_store :cookie_store, key: '_survey-web_session', :httponly => false
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
