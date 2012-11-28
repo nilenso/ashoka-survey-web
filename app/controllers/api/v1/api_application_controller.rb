3f637e0612018892df94f6f3e9c38a8e1370fce3
Timothy Andrew <mail@timothyandrew.net>
2012-11-28 09:06:29 +0530

    APIAppController#current_user_info delegates to AppController#current_user_info if the access_token isn't passed as a param

--- app/controllers/api/v1/api_application_controller.rb
+++ app/controllers/api/v1/api_application_controller.rb
@@ -1,24 +1,26 @@
 module Api
   module V1
     class APIApplicationController < ApplicationController
-      
+
       private
 
       def current_user_info
-        raw_info = access_token(params[:access_token]).get('api/me').parsed
-
-        user_info = {
-          :name => raw_info['name'],
-          :email => raw_info['email'],
-          :role => raw_info['role'],
-          :user_id => raw_info['id'],
-          :org_id => raw_info['organization_id']
-        }
-
-        user_info
+        if params[:access_token].blank?
+          super
+        else
+          raw_info = access_token(params[:access_token]).get('api/me').parsed
+          user_info = {
+            :name => raw_info['name'],
+            :email => raw_info['email'],
+            :role => raw_info['role'],
+            :user_id => raw_info['id'],
+            :org_id => raw_info['organization_id']
+          }
+          user_info
+        end
       end
 
-      def access_token(token_string)        
+      def access_token(token_string)
         OAuth2::AccessToken.new(oauth_client, token_string)
       end
     end
