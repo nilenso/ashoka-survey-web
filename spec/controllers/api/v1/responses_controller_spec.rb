1b18a17cb4dcd2adb91b9a55196d0ca8ddc8b73b
Smit Shah <who828@gmail.com>
2012-12-04 12:42:02 +0530

    Don't need to validate a survey for expiry date in the API controllers.
    Because a sync for responses taken before the expiry date might happen
    after the expiry date.
    
    Revert "Added survey expiry_date check when syncing responses"
    
    This reverts commit 6ea2e4b3928816e5708c98f08c9d5b37b63526ba.

--- spec/controllers/api/v1/responses_controller_spec.rb
+++ spec/controllers/api/v1/responses_controller_spec.rb
@@ -23,13 +23,13 @@ module Api
       end
 
       context "POST 'create'" do
-        let! (:survey) { FactoryGirl.create(:survey, :organization_id => organization_id, :expiry_date => Date.tomorrow) }
+        let (:survey) { FactoryGirl.create(:survey, :organization_id => organization_id) }
         let (:question) { FactoryGirl.create(:question) }
 
         it "creates an response" do
           resp = FactoryGirl.attributes_for(:response, :survey_id => survey.id, :answers_attributes =>{})
           expect {
-            post :create, :response => resp, :survey_id => survey.id
+            post :create, :response => resp
           }.to change { Response.count }.by 1
         end
 
@@ -42,14 +42,14 @@ module Api
 
         it "should return the newly created response with answers as JSON if it is incomplete" do
           resp = FactoryGirl.attributes_for(:response, :survey_id => survey.id, :answers_attributes =>  { '0' => {'content' => 'asdasd', 'question_id' => question.id} })
-          post :create, :response => resp, :survey_id => survey.id
+          post :create, :response => resp
           response.should be_ok
           JSON.parse(response.body).keys.should =~ Response.new.attributes.keys.unshift("answers")
         end
 
         it "should return the newly created response if it is complete" do
           resp = FactoryGirl.attributes_for(:response, :survey_id => survey.id, :status => 'complete', :answers_attributes =>  { '0' => {'content' => 'asdasd', 'question_id' => question.id} })
-          post :create, :response => resp, :survey_id => survey.id
+          post :create, :response => resp
           response.should be_ok
           JSON.parse(response.body).keys.should =~ Response.new.attributes.keys.unshift("answers")
         end
@@ -71,15 +71,6 @@ module Api
           response.should_not be_ok
           response.status.should == 400
         end
-
-        it "returns response with a bad request if survey is expired" do
-          survey = FactoryGirl.create(:survey, :organization_id => organization_id)
-          survey.update_attribute('expiry_date', Date.yesterday)
-          resp = FactoryGirl.attributes_for(:response, :survey_id => survey.id, :answers_attributes =>{})
-          post :create, :response => resp
-          response.should_not be_ok
-          response.status.should == 400
-        end
       end
 
       context "PUT 'update'" do
@@ -88,7 +79,7 @@ module Api
           question = FactoryGirl.create(:question)
           resp = FactoryGirl.create(:response, :survey => survey, :organization_id => organization_id, :user_id => 1)
           resp_attr = { :answers_attributes =>  { '0' => {'content' => 'asdasd', 'question_id' => question.id, 'updated_at' => Time.now.to_s} } }
-          put :update, :id => resp.id, :response => resp_attr, :survey_id => survey.id
+          put :update, :id => resp.id, :response => resp_attr
           response.should be_ok
           Response.find(resp.id).answers.map(&:content).should include("asdasd")
         end
@@ -121,30 +112,13 @@ module Api
         it "chooses whether to update the response status based on `updated_at`" do
           survey = FactoryGirl.create(:survey, :organization_id => organization_id)
           resp = FactoryGirl.create(:response, :organization_id => organization_id, :user_id => 1, :status => 'complete', :survey => survey)
-          put :update, :id => resp.id,:survey_id => survey.id, :response => { :status => 'incomplete', :answers_attributes => {}, :updated_at => 5.hours.ago.to_s }
+          put :update, :id => resp.id, :response => { :status => 'incomplete', :answers_attributes => {}, :updated_at => 5.hours.ago.to_s }
           resp.reload.should be_complete
         end
 
         it "returns a 410 if the response doesn't exist on the server anymore" do
           put :update, :id => 42, :response => { :status => 'incomplete', :answers_attributes => {}, :updated_at => 5.hours.ago.to_s }
           response.code.should == "410"
-        end
-
-        it "returns response with a bad request if survey is expired" do
-          survey = FactoryGirl.create(:survey, :organization_id => organization_id)
-          survey.update_attribute('expiry_date', Date.yesterday)
-          resp = FactoryGirl.create(:response, :survey_id => survey.id, :answers_attributes =>{})
-          question_1 = FactoryGirl.create(:question)
-          question_2 = FactoryGirl.create(:question)
-          resp.answers << FactoryGirl.create(:answer, :question_id => question_1.id)
-          resp.answers << FactoryGirl.create(:answer, :question_id => question_2.id)
-          resp_attr = { :answers_attributes =>
-                        { '0' => {'content' => 'newer', 'question_id' => question_1.id, 'updated_at' => 5.hours.from_now.to_s, 'id' => resp.answers.first.id},
-                          '1' => {'content' => 'older', 'question_id' => question_2.id, 'updated_at' => 5.hours.ago.to_s, 'id' => resp.answers.last.id }} }
-
-          put :update, :id => resp.id, :response => resp_attr
-          response.should_not be_ok
-          response.status.should == 400
         end
       end
 
