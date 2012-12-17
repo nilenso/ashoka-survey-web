769c2576bee1c29b1d99e453b317048d23bac6a3
Timothy Andrew <mail@timothyandrew.net>
2012-12-17 14:02:57 +0530

    Change log level to debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,6 @@ SurveyWeb::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.log_level = :debug
 end
