9ed36e639f6afffb9799c5e73d9aec9c61fea923
Timothy Andrew <mail@timothyandrew.net>
2013-01-08 10:51:49 +0530

    Move specs for Question's images from question_spec to shared_examples_for_question.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -197,42 +197,5 @@ describe Question do
     end
   end
 
-  context "for images" do
-    context "when encoding in base64" do
-      it "returns the cached image if the remote image is still uploading" do
-        question = FactoryGirl.create :question
-        question.image.stub(:cache_dir).and_return("spec/fixtures/images")
-        question.image_tmp = 'sample.jpg'
-        question.image_in_base64.should == Base64.encode64(File.read('spec/fixtures/images/sample.jpg'))
-      end
-
-      it "returns the remote image if it's done uploading" do
-        question = FactoryGirl.create :question_with_image
-        question.image_in_base64.should == Base64.encode64(File.read(question.image.thumb.path))
-      end
-    end
-
-    context "when getting the URL" do
-      it "returns the relative URL to the cached (local) image if the S3 version hasn't uploaded" do
-        question = FactoryGirl.create :question
-        question.image.stub(:cache_url).and_return("spec/fixtures/images")
-        question.image_tmp = 'sample.jpg'
-        question.image_url.should == '/spec/fixtures/images/sample.jpg'
-      end
-
-      it "returns the URL to the S3 version if it's uploaded" do
-        question = FactoryGirl.create :question_with_image
-        question.image_tmp = nil
-        question.image_url.should == question.image.url
-      end
-
-      it "takes a format (medium or thumb) which it returns only for the S3 version" do
-        question = FactoryGirl.create :question_with_image
-        question.image_tmp = nil
-        question.image_url(:thumb).should == question.image.thumb.url
-      end
-    end
-  end
-
   include_examples 'a question'
 end
--- spec/support/models/shared_examples_for_question.rb
+++ spec/support/models/shared_examples_for_question.rb
@@ -16,14 +16,45 @@ shared_examples "a question" do
     it { should allow_mass_assignment_of(:image) }
   end
 
-  context "image_url" do
-    it "returns the image_url if the question has an image" do
-      question.image = File.new(Rails.root + 'spec/fixtures/images/sample.jpg')
-      question.save
-      question.reload.image_url.should == question.image.url(:thumb)
+  context "for images" do
+    context "when encoding in base64" do
+      it "returns the cached image if the remote image is still uploading" do
+        question = FactoryGirl.create :question
+        question.image.stub(:cache_dir).and_return("spec/fixtures/images")
+        question.image_tmp = 'sample.jpg'
+        question.image_in_base64.should == Base64.encode64(File.read('spec/fixtures/images/sample.jpg'))
+      end
+
+      it "returns the remote image if it's done uploading" do
+        question = FactoryGirl.create :question_with_image
+        question.image_in_base64.should == Base64.encode64(File.read(question.image.thumb.path))
+      end
     end
-    it "returns nil if the question doesn't have an image" do
-      question.image_url.should be_nil
+
+    context "when getting the URL" do
+      it "returns the relative URL to the cached (local) image if the S3 version hasn't uploaded" do
+        question = FactoryGirl.create :question
+        question.image.stub(:cache_url).and_return("spec/fixtures/images")
+        question.image_tmp = 'sample.jpg'
+        question.image_url.should == '/spec/fixtures/images/sample.jpg'
+      end
+
+      it "returns the URL to the S3 version if it's uploaded" do
+        question = FactoryGirl.create :question_with_image
+        question.image_tmp = nil
+        question.image_url.should == question.image.url
+      end
+
+      it "takes a format (medium or thumb) which it returns only for the S3 version" do
+        question = FactoryGirl.create :question_with_image
+        question.image_tmp = nil
+        question.image_url(:thumb).should == question.image.thumb.url
+      end
+
+      it "returns nil if the question doesn't have an image" do
+        question = FactoryGirl.create :question
+        question.image_url.should be_nil
+      end
     end
   end
 end
