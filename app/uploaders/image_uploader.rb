df965410cd5148d24815199df4940736775eaa07
Timothy Andrew <mail@timothyandrew.net>
2013-01-08 10:11:19 +0530

    Set the `cache_dir` for the ImageUploader.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,6 +20,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def cache_dir
+    "#{Rails.root}/public/uploads/tmp"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
