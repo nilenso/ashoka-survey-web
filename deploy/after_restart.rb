65fcccab0b386a1819837d95a05a620ea727446b
Timothy Andrew <mail@timothyandrew.net>
2013-01-09 15:10:04 +0530

    Run the after_restart hook with the correct RAILS_ENV

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,1 +1,5 @@
-run "/data/surveyweb/current/script/delayed_job restart"
+if environment == "staging"
+  run "/data/surveyweb/current/script/delayed_job restart"
+else
+  run "RAILS_ENV=production /data/surveyweb/current/script/delayed_job restart"
+end
