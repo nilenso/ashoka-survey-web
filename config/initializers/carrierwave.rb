2d2847824bfb18de60a81c820db324c2eb0f1694
Timothy Andrew <mail@timothyandrew.net>
2013-01-10 17:48:02 +0530

    Rename the S3 bucket to `survey web`

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,6 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
     :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
   }
-  config.fog_directory  = 'surveywebstaging'                     # required
+  config.fog_directory  = 'surveyweb'                     # required
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
