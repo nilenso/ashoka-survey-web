7ceb79992c4cd4393bd4bae0946a96f5142adb27
Timothy Andrew <mail@timothyandrew.net>
2013-01-31 13:17:18 +0530

    Add model validation against nested multi-record categories.

--- app/models/multi_record_category.rb
+++ app/models/multi_record_category.rb
@@ -1,2 +1,9 @@
 class MultiRecordCategory < Category
+  validate :dont_allow_nested_multi_record, :if => :has_multi_record_ancestor?
+  
+  protected
+
+  def dont_allow_nested_multi_record
+    errors.add(:base, "No nested multi-records allowed.")
+  end
 end
--- spec/models/multi_record_category_spec.rb
+++ spec/models/multi_record_category_spec.rb
@@ -7,4 +7,11 @@ describe MultiRecordCategory do
     category.should be_a MultiRecordCategory
     category.type.should == "MultiRecordCategory"
   end
+
+  it "doesn't allow nested multi-record categories" do
+    parent_mr = MultiRecordCategory.create(:content => "mr")
+    expect do
+      MultiRecordCategory.create(:content => "child_mr", :category_id => parent_mr.id)
+    end.not_to change { MultiRecordCategory.count }
+  end
 end
