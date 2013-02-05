af7df3fdf2cba96616a3fcfda3bc2a56c801e363
Timothy Andrew <mail@timothyandrew.net>
2013-02-05 14:59:07 +0530

    Don't fetch new answers from the response in the sorted order. Sort `response.answers` based on the order of `response.sorted_answers`.
    To preserve the existing answer objects after a validation error.

--- app/decorators/response_decorator.rb
+++ app/decorators/response_decorator.rb
@@ -17,6 +17,11 @@ class ResponseDecorator < Draper::Base
     end
   end
 
+  def sort_answers
+    answer_ids = model.sorted_answers.map(&:id)
+    model.answers.sort_by! { |answer| answer_ids.index(answer.id) || 0 }
+  end
+
   private
 
 
