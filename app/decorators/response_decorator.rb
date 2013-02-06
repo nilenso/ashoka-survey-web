27fcaea8cae724cc8271562b9cecde5c7cf1f75f
Smit Shah <who828@gmail.com>
2013-02-06 12:59:22 +0530

    Removed the old method to display question_number from ResponseDecorator

--- app/decorators/response_decorator.rb
+++ app/decorators/response_decorator.rb
@@ -1,22 +1,6 @@
 class ResponseDecorator < Draper::Base
   decorates :response
 
-  def self.question_number(question)
-    if question.parent
-      sibling_elements = (question.parent.questions + question.parent.categories_with_questions).sort_by(&:order_number)
-      parent_question_number = "#{question_number(question.parent_question)}"
-      parent_question_number += "#{(question.index_of_parent_option + 65).chr}" if (question.parent_question.is_a? MultiChoiceQuestion)
-      index = ".#{sibling_elements.index(question) + 1}"
-      parent_question_number + index
-    elsif question.category
-      sibling_elements = (question.category.questions + question.category.categories_with_questions).sort_by(&:order_number)
-      "#{question_number(question.category)}.#{sibling_elements.index(question) + 1}"
-    else
-      sibling_elements = question.survey.first_level_elements
-      (sibling_elements.index(question) + 1).to_s
-    end
-  end
-
   def sort_answers
     answer_ids = model.sorted_answers.map(&:id)
     model.answers.sort_by! { |answer| answer_ids.index(answer.id) || 0 }
