956941e67973a6ec42b8bfa5d31469b5cad93627
Nivedita <nivedita@c42.in>
2013-02-19 17:06:29 +0530

    Eager loading choices along with answers while downloading excel.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -49,7 +49,6 @@ class Answer < ActiveRecord::Base
 
   def content_for_excel(server_url='')
     # TODO: Refactor these `if`s when implementing STI for the Answer model
-    return choices.map(&:content).join(", ") if question.type == 'MultiChoiceQuestion'
     return photo_url(:server_url => server_url) if question.type == 'PhotoQuestion'
     return content
   end
