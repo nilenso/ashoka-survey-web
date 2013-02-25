b011f242a7008f0fcafb9bccb3d51c1287531cac
Nivedita <nivedita@c42.in>
2013-02-25 17:19:03 +0530

    Remove cloudfront from both staging and production.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -50,7 +50,7 @@ SurveyWeb::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "//d1rlufo4k6hpl3.cloudfront.net"
+  # config.action_controller.asset_host = "//d1rlufo4k6hpl3.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # Disable delivery errors, bad email addresses will be ignored
