91c155572f8bdbe99279c93d9eb315bb755e5910
Timothy Andrew <mail@timothyandrew.net>
2013-03-12 16:05:05 +0530

    Authorize API::QuestionsController through `Survey`.

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -2,10 +2,10 @@ module Api
   module V1
     class QuestionsController < APIApplicationController
       before_filter :dont_cache
-      authorize_resource
 
       def create
         question = Question.new_question_by_type(params[:question][:type], params[:question])
+        authorize! :update, question.survey
         if question.save
           render :json => question.to_json(:methods => [:type, :has_multi_record_ancestor])
         else
@@ -15,6 +15,7 @@ module Api
 
       def update
         question = Question.find(params[:id])
+        authorize! :update, question.survey
         if question.update_attributes(params[:question])
           render :json => question.to_json(:methods => :type)
         else
@@ -23,6 +24,8 @@ module Api
       end
 
       def destroy
+        question = Question.find(params[:id])
+        authorize! :update, question.survey
         begin
           Question.destroy(params[:id])
           render :nothing => true
@@ -33,6 +36,7 @@ module Api
 
       def image_upload
         question = Question.find(params[:id])
+        authorize! :update, question.survey
         question.update_attributes({ :image => params[:image] })
         if question.save
           render :json => { :image_url => question.image_url }
@@ -43,6 +47,7 @@ module Api
 
       def index
         survey = Survey.find_by_id(params[:survey_id])
+        authorize! :read, survey
         methods = [:type, :image_url]
         methods << :image_in_base64 if request.referrer.nil?
         if survey
@@ -54,6 +59,7 @@ module Api
 
       def show
         question = Question.find_by_id(params[:id])
+        authorize! :read, question.survey
         methods = [:type, :image_url, :has_multi_record_ancestor]
         methods << :image_in_base64 if request.referrer.nil?
         if question
@@ -65,6 +71,7 @@ module Api
 
       def duplicate
         question = Question.find_by_id(params[:id])
+        authorize! :edit, question.survey
         if question && question.copy_with_order
           flash[:notice] = t("flash.question_duplicated")
         else
