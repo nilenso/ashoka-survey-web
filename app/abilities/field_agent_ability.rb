a39d3e95a57b7eb81fd3cd4318ca397b26a70a92
Timothy Andrew <mail@timothyandrew.net>
2013-03-12 18:37:11 +0530

    Added ability for the `field_agent` role.

---
+++ app/abilities/field_agent_ability.rb
@@ -0,0 +1,10 @@
+class FieldAgentAbility < Ability
+  def initialize(user_info)
+    @user_info = user_info
+
+    can :read, Survey, :survey_users => { :user_id => user_info[:user_id] }
+    can :report, Survey, :survey_users => { :user_id => user_info[:user_id] }
+    can :create, Response, :survey => { :survey_users => { :user_id => user_info[:user_id] } }
+    can :manage, Response, :user_id => user_info[:user_id]
+  end
+end
