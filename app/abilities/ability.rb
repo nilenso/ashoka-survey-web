cfca65f776dd75584bc72328fa1f10d0eb0545d1
Timothy Andrew <mail@timothyandrew.net>
2013-03-12 17:53:12 +0530

    Finish up ViewerAbility.
    - Add abilities to see responses/reports.
    - Add abilities to see surveys shared with his organisation.

--- app/abilities/ability.rb
+++ app/abilities/ability.rb
@@ -10,4 +10,26 @@ class Ability
     @user_info = user_info
     can :read, Survey, :id => nil # The root_url is Surveys#index
   end
+
+  def surveys_belonging_to_his_organization
+    { :organization_id => @user_info[:org_id] }
+  end
+
+
+  def can_perform_on_own_and_shared_surveys(action)
+    can action, Survey, sql_for_own_and_shared_surveys do |survey|
+      survey.organization_id == @user_info[:org_id] || survey.participating_organizations.find_by_organization_id(@user_info[:org_id])
+    end
+  end
+
+  private
+
+  def sql_for_own_and_shared_surveys
+    [
+        'surveys.id in (SELECT "surveys".id FROM "surveys"
+      LEFT OUTER JOIN participating_organizations ON participating_organizations.survey_id = surveys.id
+      WHERE (surveys.organization_id = ? OR participating_organizations.organization_id = ?))',
+        @user_info[:org_id], @user_info[:org_id]
+    ]
+  end
 end
--- app/abilities/viewer_ability.rb
+++ app/abilities/viewer_ability.rb
@@ -2,10 +2,8 @@ class ViewerAbility < Ability
   def initialize(user_info)
     @user_info = user_info
 
-    can :read, Survey, :organization_id => user_info[:org_id]
-    can :questions_count, Survey, :organization_id => user_info[:org_id]
-    can :read, Category, :survey => { :organization_id => user_info[:org_id] }
-    can :read, Question, :survey => { :organization_id => user_info[:org_id] }
-    can :read, Option, :question => { :survey => { :organization_id => user_info[:org_id] } }
+    can_perform_on_own_and_shared_surveys(:read)
+    can_perform_on_own_and_shared_surveys(:report)
+    can :read, Response, :organization_id => user_info[:org_id]
   end
 end
