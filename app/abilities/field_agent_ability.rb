92f0a09c9875ef5f731772a5e98eef10fe41ede9
Timothy Andrew <mail@timothyandrew.net>
2013-03-13 10:03:51 +0530

    A field_agent should be able to generate excel for his own responses.

--- app/abilities/field_agent_ability.rb
+++ app/abilities/field_agent_ability.rb
@@ -4,6 +4,7 @@ class FieldAgentAbility < Ability
 
     can :read, Survey, :survey_users => { :user_id => user_info[:user_id] }
     can :report, Survey, :survey_users => { :user_id => user_info[:user_id] }
+    can :generate_excel, Survey, :survey_users => { :user_id => user_info[:user_id] }
     can :create, Response, :survey => { :survey_users => { :user_id => user_info[:user_id] } }
     can :manage, Response, :user_id => user_info[:user_id]
   end
