04fbd3252d1d7902d0ee3f9ddb822b5c0fd90318
Nivedita <nivedita@c42.in>
2013-03-22 14:38:33 +0530

    Fixed the hint text for NumericQuestion.

--- app/decorators/numeric_question_decorator.rb
+++ app/decorators/numeric_question_decorator.rb
@@ -11,8 +11,8 @@ class NumericQuestionDecorator < QuestionDecorator
 
   def numeric_question_hint(min_value, max_value)
     return "The number should be between #{min_value} and #{max_value}" if min_value && max_value
-    return "The number should is be greater than #{max_value}" if max_value
-    return "The number should is be less than #{min_value}" if min_value
+    return "The number should is be less than #{max_value}" if max_value
+    return "The number should is be greater than #{min_value}" if min_value
     nil
   end
 end
