100f180d08eddc6a6f4d8cbdbf29e28e2d0aa4eb
Smit Shah <who828@gmail.com>
2013-03-25 02:01:34 +0530

    Allow content to be nil only for MultiChoiceQuestion

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -116,7 +116,8 @@ class Survey < ActiveRecord::Base
   end
 
   def questions_for_reports
-    questions.joins(:answers => :response).where("responses.status = 'complete' AND responses.state = 'clean' AND answers.content  <> ''").uniq
+    questions.joins(:answers => :response).where("responses.status = 'complete' AND responses.state = 'clean' AND ((answers.content  <> '' AND answers.content IS NOT NULL) OR
+                                                questions.type = 'MultiChoiceQuestion')").uniq
   end
 
   def complete_responses_count(current_ability)
