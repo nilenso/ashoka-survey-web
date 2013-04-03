622f083b501ac281a7f53ea3a476d3e235bb5e85
Nivedita <nivedita@c42.in>
2013-04-03 16:07:57 +0530

    Removed recaptcha config file, css and translations.

--- config/initializers/recaptcha.rb
+++
@@ -1,4 +0,0 @@
-Recaptcha.configure do |config|
-  config.public_key  = ENV["RECAPTCHA_PUBLIC_KEY"] || '6LcVxNgSAAAAANXZol7FzZWux77l6DYpdjCmzeD_'
-  config.private_key = ENV["RECAPTCHA_PRIVATE_KEY"] || '6LcVxNgSAAAAAOJHywBMAndj8SY3y2AIfWY5r5hp'
-end
