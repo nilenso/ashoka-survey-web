f6290fe87dbc4331e3d13409173de74f2c350da3
Timothy Andrew <mail@timothyandrew.net>
2013-04-22 17:35:22 +0530

    Remove Spork config from spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,6 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'rubygems'
-require 'spork'
-
 
 Fog.mock!
 
@@ -16,45 +14,39 @@ CarrierWave.configure do |config|
   config.enable_processing = false
 end
 
-Spork.prefork do
+LOGGED_IN_ORG_ID = 1
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-  LOGGED_IN_ORG_ID = 1
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
-  RSpec.configure do |config|
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
-    config.use_transactional_fixtures = false
-    config.infer_base_class_for_anonymous_controllers = false
-    config.order = "random"
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
 
 
-    OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:user_owner] = OmniAuth::AuthHash.new({
-      :provider => 'user_owner',
-      :uid => '12345',
-      :info => {
-        :name => 'tim',
-        :email => 'smit@smit.smit',
-        :role => 'user',
-        :org_id => '1098',
-        :org_type => 'CSO',
-        :organizations => [{:id => 123, :name => 'nid'}]
-      },
-      :credentials => { :token => "thisisatoken" }
-    })
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:user_owner] = OmniAuth::AuthHash.new({
+    :provider => 'user_owner',
+    :uid => '12345',
+    :info => {
+      :name => 'tim',
+      :email => 'smit@smit.smit',
+      :role => 'user',
+      :org_id => '1098',
+      :org_type => 'CSO',
+      :organizations => [{:id => 123, :name => 'nid'}]
+    },
+    :credentials => { :token => "thisisatoken" }
+  })
 
-    config.before(:each) do
-      DatabaseCleaner.strategy = :transaction
-      DatabaseCleaner.start
-    end
-
-    config.after(:each) do
-      DatabaseCleaner.clean
-    end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
   end
-end
 
-Spork.each_run do
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
 
 def sign_in_as(role)
