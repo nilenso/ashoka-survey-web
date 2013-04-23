63698ce0d2dcd058990124b3e8c46f148dbae07c
Timothy Andrew <mail@timothyandrew.net>
2013-04-23 18:25:39 +0530

    [#47862477] DeepSurveysController should inherit from APIApplicationController.

--- app/controllers/api/v1/deep_surveys_controller.rb
+++ app/controllers/api/v1/deep_surveys_controller.rb
@@ -1,15 +1,19 @@
-class Api::V1::DeepSurveysController < ApplicationController
-  authorize_resource :class => Survey
+module Api
+  module V1
+    class DeepSurveysController < APIApplicationController
+      authorize_resource :class => Survey
 
-  def index
-    surveys = Survey.accessible_by(current_ability)
-    render :json => surveys.active_plus(extra_survey_ids), :each_serializer => DeepSurveySerializer
-  end  
+      def index
+        surveys = Survey.accessible_by(current_ability).active_plus(extra_survey_ids)
+        render :json => surveys, :each_serializer => DeepSurveySerializer
+      end
 
-  private
-  
-  def extra_survey_ids
-    extra_survey_ids = params[:extra_surveys] || ""
-    extra_survey_ids.split(',').map(&:to_i)
+      private
+
+      def extra_survey_ids
+        extra_survey_ids = params[:extra_surveys] || ""
+        extra_survey_ids.split(',').map(&:to_i)
+      end
+    end
   end
 end
