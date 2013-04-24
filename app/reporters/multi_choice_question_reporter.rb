34349791381e6b768973ac20d41ed271cefe83df
Timothy Andrew <mail@timothyandrew.net>
2013-04-24 17:23:46 +0530

    [#47873133] Don't print as many "YES"s as there are answers. Need to deal with MR properly.

--- app/reporters/multi_choice_question_reporter.rb
+++ app/reporters/multi_choice_question_reporter.rb
@@ -7,9 +7,9 @@ class MultiChoiceQuestionReporter < QuestionReporter
     option_selections = model.options.map do |option|
       choices = Choice.where('answer_id in (?) AND option_id = ?', answers.map(&:id), option.id)
       if choices.present?
-        answers.map { "YES" }.join(", ") # Need to join in case we're in a Multi-Record
+        "YES"
       else
-        answers.map { "NO" }.join(", ")
+        "NO"
       end
     end
     [""] + option_selections
