83e736cf1546f5323ffae1e74d9aa6f5d0881d4e
Timothy Andrew <mail@timothyandrew.net>
2013-04-25 17:54:34 +0530

    [#47873133] Refactored MultiChoiceQuestiion spec.

--- app/reporters/multi_choice_question_reporter.rb
+++ app/reporters/multi_choice_question_reporter.rb
@@ -1,14 +1,16 @@
 class MultiChoiceQuestionReporter < QuestionReporter
   SELECTED_STRING     = "YES"
   NOT_SELECTED_STRING = "NO"
+  BLANK_FIELD_FOR_QUESTION = ""
 
   def header
     [super] + question.options.map(&:content)
   end
 
   def formatted_answers_for(answers, options={})
-    return [""] + model.options.map { NOT_SELECTED_STRING } if answers.empty?
-    model.options.inject([""]) do |memo, option|
+    initial_array = [BLANK_FIELD_FOR_QUESTION]
+    return initial_array + model.options.map { NOT_SELECTED_STRING } if answers.empty?
+    model.options.inject(initial_array) do |memo, option|
       selection = answers.map do |answer|
         choice = Choice.where('answer_id = ? AND option_id = ?', answer.id, option.id)
         choice.present? ? SELECTED_STRING : NOT_SELECTED_STRING
