f8f25c533552f1c86fcb54a98ed369cca5038d53
Timothy Andrew <mail@timothyandrew.net>
2013-04-26 12:51:27 +0530

    [#47873133] Add the ExcelReports::Row class.

---
+++ app/models/excel_reports/row.rb
@@ -0,0 +1,17 @@
+class ExcelReports::Row
+  attr_reader :elements
+
+  def initialize(*args)
+    @elements = args
+  end
+
+  def to_a
+    @elements.dup
+  end
+
+  def <<(elements)
+    @elements << elements
+    @elements.flatten!
+    self
+  end
+end
---
+++ spec/models/excel_reports/row_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe ExcelReports::Row do
+  it "can add elements to itself" do
+    row = ExcelReports::Row.new
+    row << "Foo" 
+    row << "Bar"
+    row.elements.should == ["Foo", "Bar"]
+  end
+
+  it "can add arrays of elements to itself" do
+    row = ExcelReports::Row.new
+    row << %w(Foo Bar)
+    row.elements.should == ["Foo", "Bar"]
+  end
+
+  it "initializes the base array with provided values" do
+    row = ExcelReports::Row.new(1,2)
+    row << "Foo"
+    row.elements.should == [1, 2, "Foo"]
+  end
+
+  it "returns a copy of its elements as an array" do
+    row = ExcelReports::Row.new(1,2)
+    array = row.to_a
+    array << 5
+    row.to_a.should_not include 5
+  end
+end
