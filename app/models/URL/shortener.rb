483fd8355031fea1a682ebb7e9e73d46ba138932
Timothy Andrew <mail@timothyandrew.net>
2013-04-30 12:15:09 +0530

    Change `class URL::Shortener` to use an explicit `module`.

--- app/models/URL/shortener.rb
+++ app/models/URL/shortener.rb
@@ -1,16 +1,18 @@
 require 'uri'
 
-class URL::Shortener
-  def initialize(url)
-    if url =~ URI::regexp
-      @url = url
-    else
-      raise ArgumentError
+module URL
+  class Shortener
+    def initialize(url)
+      if url =~ URI::regexp
+        @url = url
+      else
+        raise ArgumentError
+      end
+      @client = Bitly.client
     end
-    @client = Bitly.client
-  end
 
-  def shorten
-    @client.shorten(@url).short_url
+    def shorten
+      @client.shorten(@url).short_url
+    end
   end
 end
