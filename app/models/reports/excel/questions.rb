59ecd1d81396ffc24fbfb6c45665ae3f217c7922
Timothy Andrew <mail@timothyandrew.net>
2013-05-06 17:55:33 +0530

    [#47873081] Reports::Excel::Questions filters out private questions by default.

--- app/models/reports/excel/questions.rb
+++ app/models/reports/excel/questions.rb
@@ -2,13 +2,22 @@ class Reports::Excel::Questions
   delegate :all, :to => :questions
   attr_reader :questions
 
-  def initialize(survey)
+  def initialize(survey, current_ability)
+    @survey = survey
     @questions = survey.questions
+    @ability = current_ability
   end
 
   def build(options = {})
-    filter_private_questions = ActiveRecord::ConnectionAdapters::Column.value_to_boolean(options[:filter_private_questions])
-    if filter_private_questions
+    filter_private_questions(options)
+    self
+  end
+
+  def filter_private_questions(options = {})
+    disable_filtering = ActiveRecord::ConnectionAdapters::Column.value_to_boolean(options[:disable_filtering])
+    if disable_filtering
+      @ability.authorize!(:change_excel_filters, @survey)
+    else
       @questions = @questions.where("private != ?", true)
     end
     self
--- spec/models/reports/excel/questions_spec.rb
+++ spec/models/reports/excel/questions_spec.rb
@@ -4,21 +4,33 @@ describe Reports::Excel::Questions do
   let!(:survey) { FactoryGirl.create(:survey) }
   let!(:question) { FactoryGirl.create(:question, :survey => survey) }
   let!(:private_question) { FactoryGirl.create(:question, :survey => survey, :private => true) }
+  let(:admin_ability) { stub }
+  let(:user_ability) { stub }
+
+  before(:each) do
+    admin_ability.stub(:authorize!).and_return(true)
+    user_ability.stub(:authorize!).and_raise(CanCan::AccessDenied)
+  end
 
   it "includes all the questions of the survey" do
-    questions = Reports::Excel::Questions.new(survey)
+    questions = Reports::Excel::Questions.new(survey, admin_ability)
     questions.all.should == [question, private_question]
   end
 
   context "when filtering out private questions" do
-    it "performs the filtering if options[:filter_private_questions] is true" do
-      questions = Reports::Excel::Questions.new(survey)
-      questions.build(:filter_private_questions => true).all.should == [question]
+    it "performs the filtering by default" do
+      questions = Reports::Excel::Questions.new(survey, admin_ability)
+      questions.build.all.should == [question]
     end
 
-    it "doesn't performs the filtering if options[:filter_private_questions] is falsy" do
-      questions = Reports::Excel::Questions.new(survey)
-      questions.build.all.should == [question, private_question]
+    it "doesn't perform the filtering if options[:disable_filtering] is true" do
+      questions = Reports::Excel::Questions.new(survey, admin_ability)
+      questions.build(:disable_filtering => true).all.should == [question, private_question]
+    end
+
+    it "raises an exception if options[:disable_filtering] is true and the current user doesn't have permission to change filters for the given survey" do
+      questions = Reports::Excel::Questions.new(survey, user_ability)
+      expect { questions.build(:disable_filtering => true).all }.to raise_error
     end
   end
 end
