df2104d3f3aa3f93c6a79d4eb183ae1dfa29e07e
Timothy Andrew <mail@timothyandrew.net>
2013-05-14 18:59:39 +0530

    [#49833675] Change the finalized field for all existing elements belonging to finalised surveys to `true`

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130514100420) do
+ActiveRecord::Schema.define(:version => 20130514130610) do
 
   create_table "answers", :force => true do |t|
     t.text     "content"
