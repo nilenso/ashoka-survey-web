216acba44c5c3836a232ae79b682552838027b83
Timothy Andrew <mail@timothyandrew.net>
2013-05-17 16:12:47 +0530

    [#49574229] Authentication failure on the API controllers send down a 401 instead of redirecting.

--- spec/controllers/api/v1/api_application_controller_spec.rb
+++ spec/controllers/api/v1/api_application_controller_spec.rb
@@ -3,6 +3,35 @@ require 'spec_helper'
 module Api
   module V1
     describe APIApplicationController do
+      controller(APIApplicationController) do
+        def index
+          response = RSpec::Mocks::Mock.new(:error= => :error, :parsed => nil, :body => nil, :status => 401)
+          raise OAuth2::Error.new(response)
+        end
+      end
+
+      context "when there's an OAuth Error" do
+        it "renders an :unauthorized" do
+          get :index
+          response.should_not be_ok
+          response.status.should == 401
+        end
+      end
+
+      context "cancan - access denied" do
+        controller(APIApplicationController) do
+          def create
+            raise CanCan::AccessDenied.new("Not authorized!", :create, Survey)
+          end
+        end
+
+        it "sends down a 401 Unauthorized" do
+          sign_in_as('field_agent')
+          post :create
+          response.should_not be_ok
+          response.status.should == 401
+        end
+      end
     end
   end
 end
