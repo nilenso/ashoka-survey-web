e5448d67c7370a252040c5d227d92d8d2eba058e
Jithu Gopal <jithug87@gmail.com>
2013-06-06 18:31:48 +0530

    [#50584289] Moving access token into environment.

--- app/models/mix_panel.rb
+++ app/models/mix_panel.rb
@@ -1,7 +1,7 @@
 class MixPanel
   class << self
     def track(event, properties={}, user_info=nil, env={})
-      tracker = Mixpanel::Tracker.new("981a7a6da84c67bb7faeec8db0143beb", { :env => env })
+      tracker = Mixpanel::Tracker.new(ENV["MIXPANEL_TOKEN"], { :env => env })
       tracker.set(user_info.user_id, user_info.to_hash)
       tracker.track(event, properties.merge(:distinct_id => user_info.user_id))
     end
