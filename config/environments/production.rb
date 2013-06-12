f0a474631dea9b01dd8e1d8171ff7ca89b10195a
Timothy Andrew <mail@timothyandrew.net>
2013-06-12 17:32:02 +0530

    [#51543239] Use the engine yard provided ey account.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,16 @@ SurveyWeb::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.log_level = :debug
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :domain => EY::Config.get('base', 'domain_name'),
+    :user_name => EY::Config.get(:sendgrid, 'SENDGRID_USERNAME'),
+    :password => EY::Config.get(:sendgrid, 'SENDGRID_PASSWORD')
+  }
 end
