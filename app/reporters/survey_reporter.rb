bf12ee22875f6e9a65fdb2396673f1da52366b3e
Timothy Andrew <mail@timothyandrew.net>
2013-07-12 11:43:13 +0530

    [#47864113] Style the responses dashboard (show) page.

--- app/reporters/survey_reporter.rb
+++ app/reporters/survey_reporter.rb
@@ -3,11 +3,21 @@ class SurveyReporter
     @survey = survey
   end
 
-  def incomplete_responses_per_survey_per_user_grouped_by_month(user_id)
+  def monthly_incomplete_responses_for(user_id)
     @survey.responses.where(:user_id => user_id, :status => Response::Status::INCOMPLETE).count(:group => "to_char(created_at, 'Mon YYYY')")
   end
 
-  def complete_responses_per_survey_per_user_grouped_by_completion_month(user_id)
+  def monthly_complete_responses_for(user_id)
     @survey.responses.where(:user_id => user_id, :status => Response::Status::COMPLETE).count(:group => "to_char(completed_at, 'Mon YYYY')")
+  end
+
+  def response_counts_for(user_id)
+    incomplete_count = monthly_incomplete_responses_for(user_id)
+    complete_count = monthly_complete_responses_for(user_id)
+    by_month = (incomplete_count.keys + complete_count.keys).uniq.map do |key|
+      [key, { Response::Status::INCOMPLETE => incomplete_count[key] || 0,
+              Response::Status::COMPLETE => complete_count[key] || 0 }]
+    end
+    Hash[by_month]
   end
 end
--- spec/reporters/survey_reporter_spec.rb
+++ spec/reporters/survey_reporter_spec.rb
@@ -8,14 +8,14 @@ describe SurveyReporter do
     it 'returns the number of incomplete responses grouped by the month it was created' do
       response_1 = Timecop.freeze("2013-05-05") { FactoryGirl.create(:response, :incomplete, :user_id => user.id, :survey => survey) }
       response_2 = Timecop.freeze("2013-06-06") { FactoryGirl.create(:response, :incomplete, :user_id => user.id, :survey => survey) }
-      report = SurveyReporter.new(survey).incomplete_responses_per_survey_per_user_grouped_by_month(user.id)
+      report = SurveyReporter.new(survey).monthly_incomplete_responses_for(user.id)
       report.should == {"May 2013" => 1, "Jun 2013" => 1}
     end
 
     it "doesn't take complete responses into account" do
       complete_responses = FactoryGirl.create_list(:response, 5, :complete, :user_id => user.id, :survey => survey)
       incomplete_response = FactoryGirl.create(:response, :incomplete, :user_id => user.id, :survey => survey)
-      report = SurveyReporter.new(survey).incomplete_responses_per_survey_per_user_grouped_by_month(user.id)
+      report = SurveyReporter.new(survey).monthly_incomplete_responses_for(user.id)
       report.values.reduce(:+).should == 1
     end
 
@@ -24,7 +24,7 @@ describe SurveyReporter do
       second_user = FactoryGirl.build(:user)
       first_users_response = FactoryGirl.create(:response, :incomplete, :user_id => first_user.id, :survey => survey)
       second_users_responses = FactoryGirl.create_list(:response, 5, :incomplete, :user_id => second_user.id, :survey => survey)
-      report = SurveyReporter.new(survey).incomplete_responses_per_survey_per_user_grouped_by_month(first_user.id)
+      report = SurveyReporter.new(survey).monthly_incomplete_responses_for(first_user.id)
       report.values.reduce(:+).should == 1
     end
 
@@ -33,12 +33,12 @@ describe SurveyReporter do
       second_survey = FactoryGirl.create(:survey)
       first_surveys_response = FactoryGirl.create(:response, :incomplete, :user_id => user.id, :survey => first_survey)
       second_surveys_responses = FactoryGirl.create_list(:response, 5, :incomplete, :user_id => user.id, :survey => second_survey)
-      report = SurveyReporter.new(first_survey).incomplete_responses_per_survey_per_user_grouped_by_month(user.id)
+      report = SurveyReporter.new(first_survey).monthly_incomplete_responses_for(user.id)
       report.values.reduce(:+).should == 1
     end
   end
 
-  context "for incomplete responses" do
+  context "for complete responses" do
     it "returns the number of complete responses per survey per user grouped by the month it was completed" do
       user = FactoryGirl.build(:user)
       survey = FactoryGirl.create(:survey)
@@ -46,7 +46,7 @@ describe SurveyReporter do
       response_2 = Timecop.freeze("2011-05-05") { FactoryGirl.create(:response, :incomplete, :user_id => user.id, :survey => survey) }
       Timecop.freeze("2013-05-05") { response_1.status = Response::Status::COMPLETE; response_1.save }
       Timecop.freeze("2013-06-06") { response_2.status = Response::Status::COMPLETE; response_2.save }
-      report = SurveyReporter.new(survey).complete_responses_per_survey_per_user_grouped_by_completion_month(user.id)
+      report = SurveyReporter.new(survey).monthly_complete_responses_for(user.id)
       report.should == {"May 2013" => 1, "Jun 2013" => 1}
     end
 
@@ -55,7 +55,7 @@ describe SurveyReporter do
       survey = FactoryGirl.create(:survey)
       complete_response = FactoryGirl.create(:response, :complete, :user_id => user.id, :survey => survey)
       incomplete_response = FactoryGirl.create(:response, :incomplete, :user_id => user.id, :survey => survey)
-      report = SurveyReporter.new(survey).complete_responses_per_survey_per_user_grouped_by_completion_month(user.id)
+      report = SurveyReporter.new(survey).monthly_complete_responses_for(user.id)
       report.values.reduce(:+).should == 1
     end
 
@@ -64,7 +64,7 @@ describe SurveyReporter do
       second_user = FactoryGirl.build(:user)
       first_users_response = FactoryGirl.create(:response, :complete, :user_id => first_user.id, :survey => survey)
       second_users_responses = FactoryGirl.create_list(:response, 5, :complete, :user_id => second_user.id, :survey => survey)
-      report = SurveyReporter.new(survey).complete_responses_per_survey_per_user_grouped_by_completion_month(first_user.id)
+      report = SurveyReporter.new(survey).monthly_complete_responses_for(first_user.id)
       report.values.reduce(:+).should == 1
     end
 
@@ -73,8 +73,23 @@ describe SurveyReporter do
       second_survey = FactoryGirl.create(:survey)
       first_surveys_response = FactoryGirl.create(:response, :complete, :user_id => user.id, :survey => first_survey)
       second_surveys_responses = FactoryGirl.create_list(:response, 5, :complete, :user_id => user.id, :survey => second_survey)
-      report = SurveyReporter.new(first_survey).complete_responses_per_survey_per_user_grouped_by_completion_month(user.id)
+      report = SurveyReporter.new(first_survey).monthly_complete_responses_for(user.id)
       report.values.reduce(:+).should == 1
+    end
+  end
+
+  context "when combining data for complete and incomplete responses" do
+    it "returns a hash of dates mapped to a hash containing incomplete and complete response counts" do
+      Timecop.freeze("2013-05-05") { FactoryGirl.create(:response, :complete, :user_id => user.id, :survey => survey) }
+      Timecop.freeze("2013-05-05") { FactoryGirl.create_list(:response, 3, :incomplete, :user_id => user.id, :survey => survey) }
+      report = SurveyReporter.new(survey).response_counts_for(user.id)
+      report.should == { "May 2013" => { Response::Status::INCOMPLETE => 3, Response::Status::COMPLETE => 1 }}
+    end
+
+    it "returns a zero count when there are no responses" do
+      Timecop.freeze("2013-05-05") { FactoryGirl.create(:response, :complete, :user_id => user.id, :survey => survey) }
+      report = SurveyReporter.new(survey).response_counts_for(user.id)
+      report.should == { "May 2013" => { Response::Status::INCOMPLETE => 0, Response::Status::COMPLETE => 1 }}
     end
   end
 end
