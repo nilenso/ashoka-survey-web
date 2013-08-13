54785149853c314aa9d9d7715d68599a6d25400f
Akshay Gupta <kitallis@gmail.com>
2013-08-13 21:43:30 +0530

    temporarily disabling assets debug as requests are taking too much time to load

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ SurveyWeb::Application.configure do
 
   config.assets.initialize_on_precompile = true
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
   Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 
